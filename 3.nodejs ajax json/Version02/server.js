2a51aedf7dbb73e9f6452d2de7d83c35d363619e
SUNNERCMS <309355484@qq.com>
2019-05-09 19:40:47 +0800

    add router and mock

--- 3.nodejs ajax json/Version02/server.js
+++ 3.nodejs ajax json/Version02/server.js
@@ -1,23 +1,13 @@
 var http = require("http");
-var fs = require("fs");
+var url=require("url");
 
-function onRequest(request, response){
-  console.log("Request received.");
-  // 用来异步读取data.json文件中的内容
-  fs.readFile("data.json",function(error,file){
-    if(error){
-        response.writeHead(500,{"Content-Type":'application/json','charset':'utf-8','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'});//可以解决跨域的请求
-        response.write(error+'\n');
-        response.end();
-    }else{
-        response.writeHead(200,{"Content-Type":'application/json','charset':'utf-8','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'});//可以解决跨域的请求
-        response.write(file);
-        response.end();
-        console.log("fanhui:"+file);
-        console.log(typeof file);
-    }
-});
+function start(route,pathMathObj){
+    function onRequest(request, response){
+        console.log("Request received.");
+        var pathname=url.parse(request.url).pathname;
+        route(pathname,response,pathMathObj);
+      }
+      http.createServer(onRequest).listen(8888);
+      console.log("Server has started.port on 8888\n");
 }
-
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.port on 8888\n");
+exports.start=start;
